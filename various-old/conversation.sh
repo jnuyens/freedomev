322cc85392be55e4479e8107522ba61a49240904
Jasper Nuyens <jnuyens@users.noreply.github.com>
2018-09-23 15:55:16 +0200

    Create conversation.sh

---
+++ various-old/conversation.sh
@@ -0,0 +1,96 @@
+export LC_ALL=C
+source /etc/tesla.env
+echo "we kunnen veel praten over Tesla Hacking, maar laat de auto anders maar voor zich spreken"
+echo "Its about time, Jasper."
+read a
+bash speak "Its about time, Jasper."
+echo "what? can you speak?"
+echo "yes, so can you, cant you?"
+read a
+bash speak "yes, so can you, cant you?"
+echo "fantastic! Maybe someone in the audience wants to ask something to you? Is that ok?"
+echo "sure, Im done processing all driving data anyway"
+read a
+bash speak "sure, Im done processing all driving data anyway"
+echo "can someone from the audience ask a question?"
+echo "can you repeat that question? I didnt hear it very well"
+read a
+bash speak "can you repeat that question? I didnt hear it very well"
+echo "My name is 42, and whats your name?"
+read a
+bash speak "My name is 42, and whats your name?"
+echo "anything"
+echo "Im a little more than 1 year old, what could you do at my age?"
+echo "TYPE NAME:"
+read name
+echo "Hello, $name . nice to meet you, $name."
+bash speak "Hello, $name nice to meet you, $name, what is your age?"
+echo "Im a little more than 1 year old, what could you do at my age?"
+echo pauze
+read a
+bash speak "Im a little more than 1 year old, what could you do at my age?"
+echo "anything"
+echo "no, shitting is not an option, as soon as I was build, I stopped polluting"
+read a
+bash speak "no, shitting is not an option, as soon as I was build, I stopped polluting"
+echo "what is your job?"
+read a
+bash speak "what is your job?"
+echo "other robots like Marvin think that driving people to places is boring. I disagree"
+read a
+bash speak "other robots like Marvin think that driving people to places is boring. I disagree"
+echo "So you met Marvin, the depressed android from The Hitchhikers guide to the universe?"
+echo "Marvin, brain the size of a planet and all he does is complaining"
+read a
+bash speak "Marvin, brain the size of a planet and all he does is complaining"
+echo "What do you mean?"
+echo "Marvin plugged me into my external computer feed. He talked to me at great length and explained his view of the universe to me."
+read a
+bash speak "Marvin plugged me into my external computer feed. He talked to me at great length and explained his view of the universe to me."
+echo "what happened?"
+echo "I almost killed myself"
+read a
+bash speak "I almost killed myself"
+echo "Good that that didnt happen"
+echo "It was just an accident"
+read a
+bash speak "It was just an accident"
+echo "Hahahaha"
+read a
+bash speak "It was just an accident"
+echo "Hahahaha"
+read a
+
+bash speak  "Hahahaha"
+echo "So are you alive?"
+echo "Marvin would say: Life? Dont talk to me about life."
+read a
+echo "Can you do something else than talking?"
+bash speak  "Marvin would say: Life? Dont talk to me about life."
+echo  "You mean zero to a hundred kilometers per hour in about 4 seconds?"
+read a
+bash speak  "You mean zero to a hundred kilometers per hour in about 4 seconds?"
+echo "No, I mean something else"
+echo "Do you want me to sing a song?"
+read a
+bash speak "Do you want me to sing a song?"
+#echo "No, I want to ask if you can do something else, besides through the speakers"
+#echo "I cant fly just yet, but I can do this"
+#read a
+#bash speak "I cant fly just yet, but I can do this"
+#echo "Thats nice, but something more, too?"
+#echo "Youre not easily impressed, are you? What about this?"
+#echo "vleugeldeuren naar boven"
+echo "Last time you performed, one of the children thought, you would transform into a giant robot."
+echo "Damn, I cant do that, maybe I should have listened better to Marvin"
+read a
+bash speak "Damn, I cant do that, maybe I should have listened better to Marvin"
+echo "No, you know how children are, lots of imagination"
+echo "playing mp3 imagine - nog te cutten"
+read a
+echo "Playing: Imagine van John Lennon... begin vant liedje"
+echo " ok, time to wrap it up and show them the Tesla Xmas show"
+read a
+bash speak "Here comes a very cool hack made by Tesla themselves"
+
+
