23437f6a56ec24f368f9f8c295512c3eebe9eca7
Jo Giraerts <jo@rombit.be>
2019-01-30 17:06:23 +0100

    typo fix for REQUEST -> _REQUEST

--- var/www/html/controller.php
+++ var/www/html/controller.php
@@ -2,18 +2,20 @@
 switch ($_REQUEST['command']) {
   case 'privacy':
     system("sleep 2");
-    return "privacy on";
+    echo "privacy on";
   case 'devmode':
     system("sleep 4");
-    return "devmode on";
+    echo "devmode on";
   case 'nosleep':
     system("sleep 6");
-    return "nosleep on";
+    echo "nosleep on";
   case 'hotspot':
     system("sleep 3");
-    return "hotspot on";
+    echo "hotspot on";
   case "update_lvs":
-    return file_get_contents("http://192.168.90.100:4035/set_data_value?name={$REQUEST['key']}&value={$REQUEST['value']}");
+//    echo "http://192.168.90.100:4035/set_data_value?name={$_REQUEST['key']}&value={$_REQUEST['value']}";
+//    die;
+    return file_get_contents("http://192.168.90.100:4035/set_data_value?name={$_REQUEST['key']}&value={$_REQUEST['value']}");
 
   // You can add more actions here
 
